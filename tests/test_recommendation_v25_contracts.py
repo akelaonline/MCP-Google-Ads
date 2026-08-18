eda1853f346a3076e50ded9ef2471bc1409f2819
Alejandro Jose <76195052+akelaonline@users.noreply.github.com>
2026-08-18 15:55:51 -0300

    style: sort recommendation contract test imports

--- tests/test_recommendation_v25_contracts.py
+++ tests/test_recommendation_v25_contracts.py
@@ -4,10 +4,10 @@ from __future__ import annotations
 
 from types import SimpleNamespace
 
+from conftest import FakeAuditLog, FakeMcp
 from google.ads.googleads.client import GoogleAdsClient
 from google.oauth2.credentials import Credentials
 
-from conftest import FakeAuditLog, FakeMcp
 from google_ads_mcp.context import AppContext
 from google_ads_mcp.safety import SafetyLayer
 from google_ads_mcp.tools import recommendations
